5af6ea8e7e601ff44e687b4b56a19084cc5b86fa
Mateusz <mateusz@laptop.lan>
2012-11-25 00:23:50 +0100

    loading of header

--- main.cpp
+++ main.cpp
@@ -1,11 +1,60 @@
 #include <QtCore/QCoreApplication>
 #include <QByteArray>
+#include <QDataStream>
+#include <QFile>
 #include <stdio.h>
+#include <stdexcept>
 
 
 char testInt[] = { 0x04, 0xFF, 0xB0, 0x5, 0x3, 0x4};
 
 QByteArray ba(QByteArray::fromRawData(testInt, sizeof(testInt)));
+
+struct Header {
+    bool big_endian;
+    quint32 file_size;
+    QString root_name;
+    quint8 version;
+
+    bool readHeader(QIODevice & dev) {
+        QDataStream str(&dev); // by default big endian mode
+        quint16 endi = 0;
+        str >> endi;
+        big_endian = endi == 0x00BF;
+        if (! big_endian)
+            throw std::runtime_error("Invalid endiannes.");
+
+        str >> file_size;
+        if (file_size + 6 != dev.size()) // to fit rest of data
+            throw std::runtime_error("File size invalid.");
+
+        char sign[10];
+        static const char sign_valid[10]={'T','C','S', 'O', 0, 4, 0, 0, 0, 0};
+        str.readRawData(sign, sizeof(sign));
+
+        if (memcmp(sign, sign_valid, sizeof(sign))) // invalid signature
+            throw std::runtime_error("No valid signature found.");
+
+        quint16 root_len = 0;
+        str >> root_len;
+        {
+            char name[root_len];
+            str.readRawData(name, root_len);
+            root_name = QString::fromUtf8(name, root_len);
+        }
+        str.skipRawData(3); // 3 bytes of crap
+        str >> version;
+        printf("Version: %x\n", version);
+        if (version !=3) // Support only amfv3
+            throw std::runtime_error("Unsupported version of AMF");
+
+        return str.status() == QDataStream::Ok;
+    }
+
+    bool writeHeader(QIODevice * dev) {
+        return true;
+    }
+};
 
 bool serializeInt(int from, QByteArray & to) {
     if (from > (2 << 29) - 1) // overflow
@@ -73,10 +122,16 @@ int main(int argc, char *argv[])
     printf("-> %i\n", i);
 
     serializeInt(0x34, ba);
-    serializeInt(0x34a, ba);
-    serializeInt(0x34bb, ba);
-    serializeInt(0x34bbc, ba);
-    serializeInt(0x34bbccd, ba);
+
+    QFile in("jacksmith_1.sol"), out("test.sol");
+    in.open(QIODevice::ReadOnly);
+    out.open(QIODevice::WriteOnly);
+
+    Header h;
+
+    printf("%d\n", h.readHeader(in));
+
+    printf("Loaded %d bytes. Root name %s.\n", h.file_size, h.root_name.toAscii().constData());
 
     return 0;//a.exec();
 }
